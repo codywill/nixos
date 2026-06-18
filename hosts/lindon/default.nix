b53d663d4bbda1bf582c774329788670ee152c36
cody <24554738+codywill@users.noreply.github.com>
2026-06-18 15:34:30 -0700

    Clean up

--- hosts/lindon/default.nix
+++ hosts/lindon/default.nix
@@ -3,17 +3,13 @@
     ./hardware-configuration.nix
     ../../modules/nixos
     ../../users/cody
-
-    # ../common/global
-    # ../common/users/cody
-    #
-    # ../common/optional/docker.nix
-    # ../common/optional/nvidia-graphics.nix
-    # ../common/optional/xbox-controller.nix
   ];
 
   networking.hostName = "lindon";
+
+  docker.enable = true;
   nvidia-gpu.enable = true;
+  hardware.xone.enable = true;
 
   home-manager.users.cody = import ../../users/cody/home.nix;
 
@@ -22,21 +18,4 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
-
-  # # Configure the X11 windowing system.
-  # services.xserver = {
-  #   enable = true;
-  #
-  #   # Configure keymap in X11
-  #   xkb = {
-  #     layout = "us";
-  #     variant = "";
-  #   };
-  # };
-  #
-  # # Enable the GNOME Desktop Environment.
-  # services.displayManager.gdm.enable = true;
-  # services.desktopManager.gnome.enable = true;
-  #
-  # system.stateVersion = "26.05";
 }
