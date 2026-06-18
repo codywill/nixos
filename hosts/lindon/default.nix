14964e73e619c4bf2c122e70f3677cc4d8dc0110
cody <24554738+codywill@users.noreply.github.com>
2026-06-18 15:32:33 -0700

    Restructured lindon

--- hosts/lindon/default.nix
+++ hosts/lindon/default.nix
@@ -1,18 +1,21 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ../../modules/nixos
+    ../../users/cody
 
-    ../common/global
-    ../common/users/cody
-
-    ../common/optional/docker.nix
-    ../common/optional/nvidia-graphics.nix
-    ../common/optional/xbox-controller.nix
+    # ../common/global
+    # ../common/users/cody
+    #
+    # ../common/optional/docker.nix
+    # ../common/optional/nvidia-graphics.nix
+    # ../common/optional/xbox-controller.nix
   ];
 
-  networking = {
-    hostName = "lindon";
-  };
+  networking.hostName = "lindon";
+  nvidia-gpu.enable = true;
+
+  home-manager.users.cody = import ../../users/cody/home.nix;
 
   programs.steam = {
     enable = true;
@@ -20,20 +23,20 @@
     dedicatedServer.openFirewall = true;
   };
 
-  # Configure the X11 windowing system.
-  services.xserver = {
-    enable = true;
-
-    # Configure keymap in X11
-    xkb = {
-      layout = "us";
-      variant = "";
-    };
-  };
-
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-
-  system.stateVersion = "26.05";
+  # # Configure the X11 windowing system.
+  # services.xserver = {
+  #   enable = true;
+  #
+  #   # Configure keymap in X11
+  #   xkb = {
+  #     layout = "us";
+  #     variant = "";
+  #   };
+  # };
+  #
+  # # Enable the GNOME Desktop Environment.
+  # services.displayManager.gdm.enable = true;
+  # services.desktopManager.gnome.enable = true;
+  #
+  # system.stateVersion = "26.05";
 }
